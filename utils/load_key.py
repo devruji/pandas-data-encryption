94ad7fc1373f94762dd305b38f2a5a71f4516d15
bossruji <en.rujikorn@gmail.com>
2024-12-13 15:42:29 +0700

    initial commit

---
+++ utils/load_key.py
@@ -0,0 +1,16 @@
+def load_key(key_name: str = "DataKey.key") -> bytes:
+    """
+    The function `load_key` reads and returns the content of a binary file named
+    specified by the parameter `key_name` located in the "./keys" directory.
+
+    :param key_name: The name of the key file to read. Defaults to "DataKey.key".
+    :return: The function `load_key()` is returning the content of the file
+    "./keys/<key_name>" as a binary string.
+    """
+    # ?: Open the key file in binary read mode
+    with open(f"./keys/{key_name}", "rb") as key_file:
+        # ?: Read the content of the key file
+        key_content = key_file.read()
+
+        # ?; Return the content of the key file
+        return key_content
